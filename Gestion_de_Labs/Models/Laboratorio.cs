7d45dc8424f6b1fab19ba5942d81233c8558e93d
Andrés Gutiérrez <andgr1987@hotmail.com>
2025-11-01 17:46:33 -0600

    Conflicto de archivos
    —Resuelto conflicto en Laboratorio.cs

--- Gestion_de_Labs/Models/Laboratorio.cs
+++ Gestion_de_Labs/Models/Laboratorio.cs
@@ -1,10 +1,12 @@
-﻿
+using System.ComponentModel.DataAnnotations;
 namespace Gestion_de_Labs.Models
 {
 
     public class Laboratorio
     {
 
+
+        [Key]
         public int Laboratorio_Id { get; set; }
         public string Nombre { get; set; }
         public int Capacidad { get; set; }
