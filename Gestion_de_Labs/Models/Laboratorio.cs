2c98b8ca669b70fcf4c6a7971e2043e5779ab5d2
Andrés Gutiérrez <andgr1987@hotmail.com>
2025-10-31 19:11:04 -0600

    Añadido de Nugets
    Nugets!

--- Gestion_de_Labs/Models/Laboratorio.cs
+++ Gestion_de_Labs/Models/Laboratorio.cs
@@ -2,5 +2,9 @@
 {
     public class Laboratorio
     {
+        public int Laboratorio_Id { get; set; }
+        public string Nombre { get; set; }
+        public int Capacidad { get; set; }
+        public string Responsable { get; set; }
     }
 }
