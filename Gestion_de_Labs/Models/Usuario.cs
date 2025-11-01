c732a9be86d6fe8e1480162b88defd9089d0ac60
jtorres90443 <jtorres90443@ufide.ac.cr>
2025-10-31 19:12:01 -0600

    UsuarioModel

--- Gestion_de_Labs/Models/Usuario.cs
+++ Gestion_de_Labs/Models/Usuario.cs
@@ -2,5 +2,26 @@
 {
     public class Usuario
     {
+        public int Usuario_Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Tipo_Usuario_Id { get; set; }
+
+        public string Tipo_Usuario_Nombre
+        {
+            get
+            {
+                switch (Especialidad)
+                {
+                    case 1:return "Estudiante";
+                    case 2:return "Profesor";
+                    default:return "Otro";
+                }
+}
+        }
+
+        public string Correo { get; set; }
+
     }
 }
